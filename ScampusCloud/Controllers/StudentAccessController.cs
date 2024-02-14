720e95b14161994f07095ba387e78333625c92a4
Viral <istiyaksaiyed35@gmail.com>
2024-02-14 23:55:10 +0530

    #Student access page

---
+++ ScampusCloud/Controllers/StudentAccessController.cs
@@ -0,0 +1,52 @@
+﻿using ScampusCloud.Models;
+using ScampusCloud.Repository.Student;
+using ScampusCloud.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScampusCloud.Controllers
+{
+    [SessionTimeoutAttribute]
+    public class StudentAccessController : Controller
+    {
+        #region Variable Declaration
+        private readonly StudentRepository _StudentRepository;
+        StudentAccessGroupModel _StudentAccessGroupModel = new StudentAccessGroupModel();
+        #endregion
+
+        public StudentAccessController()
+        {
+                _StudentRepository = new StudentRepository();
+        }
+
+        // GET: StudentAccess
+        public ActionResult StudentAccess()
+        {
+            _StudentAccessGroupModel.lstCampus = BindCampusDropdown(SessionManager.CompanyId.ToString());
+            _StudentAccessGroupModel.lstYear = BindYearDropDown(SessionManager.CompanyId.ToString());
+            _StudentAccessGroupModel.lstAdmissionType = BindAdmissionTypeDropDown(SessionManager.CompanyId.ToString());
+            return View(_StudentAccessGroupModel);
+        }
+        private List<SelectListItem> BindCampusDropdown(string CompanyId)
+        {
+            List<SelectListItem> drpList = new List<SelectListItem>();
+            drpList = _StudentRepository.BindCampusDropdown(CompanyId);
+            return drpList;
+        }
+        private List<SelectListItem> BindYearDropDown(string CompanyId)
+        {
+            List<SelectListItem> drpList = new List<SelectListItem>();
+            drpList = _StudentRepository.BindYearDropDown(CompanyId);
+            return drpList;
+        }
+        private List<SelectListItem> BindAdmissionTypeDropDown(string CompanyId)
+        {
+            List<SelectListItem> drpList = new List<SelectListItem>();
+            drpList = _StudentRepository.BindAdmissionTypeDropDown(CompanyId);
+            return drpList;
+        }
+    }
+}
---
+++ ScampusCloud/Models/StudentAccessGroupModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ScampusCloud.Models
+{
+    public class StudentAccessGroupModel
+    {
+        public int? Id { get; set; }
+     
+        [Column(TypeName = "varchar")]
+        public string StudentId { get; set; }
+        [Required(ErrorMessage = "Please enter name")]
+        [Column(TypeName = "varchar")]
+        public string Name { get; set; }
+        public Guid? CompanyId { get; set; }
+        public int? AccessGroupId { get; set; }
+        public int? SessionId { get; set; }
+        public int? BlockId { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime dtCreatedDate { get; set; }
+        public DateTime dtModifiedDate { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public int CollageId { get; set; }
+        public List<SelectListItem> lstCollage { get; set; }
+        public int DepartmentId { get; set; }
+        public List<SelectListItem> lstDepartment { get; set; }
+        public int CampusId { get; set; }
+        public List<SelectListItem> lstCampus { get; set; }
+        public int YearId { get; set; }
+        public List<SelectListItem> lstYear { get; set; }
+        public int AdmissionTypeId { get; set; }
+        public List<SelectListItem> lstAdmissionType { get; set; }
+        public string Gender { get; set; }
+    }
+}
